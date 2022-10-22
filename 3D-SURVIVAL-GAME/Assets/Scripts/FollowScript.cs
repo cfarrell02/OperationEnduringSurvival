96c662abc593292f6e130b0257918ba2873038bb
Cian Farrell <cianfarrell02@gmail.com>
2022-10-22 18:50:50 +0100

    Basic Zombie animations added

--- 3D-SURVIVAL-GAME/Assets/Scripts/FollowScript.cs
+++ 3D-SURVIVAL-GAME/Assets/Scripts/FollowScript.cs
@@ -9,13 +9,20 @@ namespace UnityStandardAssets.Utility
         [SerializeField] GameObject target;
         [SerializeField] private float speed = 1.5f;
         [SerializeField] private float detectionDistance = 50f;
-
-
+        private Animator animator;
+        void Start()
+        {
+            animator = GetComponentInChildren<Animator>();
+        }
         void Update()
         {
             float distance = Vector3.Distance(this.transform.position, target.transform.position);
-            if(distance<detectionDistance)
-            transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+            if (distance < detectionDistance)
+            {
+                transform.LookAt(target.transform);
+                transform.position = Vector3.MoveTowards(transform.position, target.transform.position, speed * Time.deltaTime);
+                animator.SetBool("Walking", true);
+            }
         }
     }
 }
