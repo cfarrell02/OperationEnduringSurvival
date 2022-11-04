6409d9de550c0ddcf7624c750b9a7568f061904e
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-11-04 22:00:49 +0000

    Updated second level

--- 3D-SURVIVAL-GAME/Assets/Scripts/BackgroundAudio.cs
+++ 3D-SURVIVAL-GAME/Assets/Scripts/BackgroundAudio.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class BackgroundAudio : MonoBehaviour
@@ -7,11 +8,21 @@ public class BackgroundAudio : MonoBehaviour
     private float timeUntilNextSound = 5f, startTime;
     private AudioSource audioSource;
     [SerializeField] private AudioClip zombieNoise;
+    [SerializeField] private AudioClip[] levelNoise;
+    private int levelIndex;
     // Start is called before the first frame update
     void Start()
     {
+        levelIndex = SceneManager.GetActiveScene().buildIndex;
         audioSource = GetComponent<AudioSource>();
         startTime = Time.time;
+        print(levelIndex - 1 <= levelNoise.Length);
+        if (levelIndex - 1 <= levelNoise.Length) {
+            audioSource.clip = levelNoise[levelIndex - 1];
+            audioSource.Play();
+        }
+        else
+            audioSource.clip = null;
     }
 
     // Update is called once per frame
