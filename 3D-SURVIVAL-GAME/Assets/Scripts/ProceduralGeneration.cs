2dcd2db504ca2ae38c5a3da35b29da234d173728
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-11-08 16:54:37 +0000

    Beginning of procedural generation added

---
+++ 3D-SURVIVAL-GAME/Assets/Scripts/ProceduralGeneration.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProceduralGeneration : MonoBehaviour
+{
+    [SerializeField] private GameObject crossRoads, straightRoad, noRoad;
+    private enum blockType {crossRoads,straightRoad,noRoad};
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
