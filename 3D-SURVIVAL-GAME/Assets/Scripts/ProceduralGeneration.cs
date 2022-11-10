304710e242d0b01531baebb423e18283e16a4321
Cian Farrell <73341912+cfarrell02@users.noreply.github.com>
2022-11-10 11:29:29 +0000

    Procedural Generation Enhanced

--- 3D-SURVIVAL-GAME/Assets/Scripts/ProceduralGeneration.cs
+++ 3D-SURVIVAL-GAME/Assets/Scripts/ProceduralGeneration.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ProceduralGeneration : MonoBehaviour
 {
-    [SerializeField] private GameObject player, crossRoads, straightRoad, noRoad, bend;
-    private GameObject[] blockTypes;
+    [SerializeField] private GameObject player;
+    [SerializeField] private GameObject[] blockTypes;
 
     private int radius = 5, planeOffset = 25;
 
@@ -23,33 +23,31 @@ public class ProceduralGeneration : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        blockTypes = new GameObject[4];
-        blockTypes[0] = crossRoads;
-        blockTypes[1] = straightRoad;
-        blockTypes[2] = noRoad;
-        blockTypes[3] = bend;
+       // blockTypes = new GameObject[4];
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(startPos == Vector3.zero || HasPlayerMoved())
+        if (startPos == Vector3.zero || HasPlayerMoved())
         {
-            for(int x = -radius; x< radius; x++)
+            for (int x = -radius; x < radius; x++)
             {
-                for(int z = -radius; z<radius; z++)
+                for (int z = -radius; z < radius; z++)
                 {
                     Vector3 pos = new Vector3((x * planeOffset + XPlayerLocation),
                         0,
                         z * planeOffset + ZPlayerLocation);
 
+
                     if (!tilePlane.Contains(pos))
                     {
                         //int index = Mathf.Abs(z + radius) % 4;
-                        int index = ((int)player.transform.position.x) % 4;
-                        print(index);
-                        GameObject _plane = Instantiate(blockTypes[index], pos, Quaternion.identity);
-                        tilePlane.Add(pos, _plane);
+                        int index = Random.Range(0,4);
+                        int randomAngle = Random.Range(1, 4) * 90;
+                        RoadType road = new RoadType(blockTypes[index],pos,randomAngle);
+                        GameObject _plane = road.InstatiateRoad();
+                        tilePlane.Add(pos, road);
                     }
                 }
             }
@@ -59,11 +57,118 @@ public class ProceduralGeneration : MonoBehaviour
 
     bool HasPlayerMoved()
     {
-        if(Mathf.Abs(XPlayerMove) >= planeOffset || Mathf.Abs(ZPlayerMove) >= planeOffset)
+        if (Mathf.Abs(XPlayerMove) >= planeOffset || Mathf.Abs(ZPlayerMove) >= planeOffset)
         {
             return true;
         }
         return false;
+    }
+
+    public class RoadType{
+
+        private GameObject gameObject;
+        public string name;
+        private Vector3 position;
+        public float rotation;
+        private Hashtable connections;
+
+
+        public RoadType(GameObject gameObject, Vector3 pos , float rotation )
+        {
+            this.gameObject = gameObject;
+            this.position = pos;
+            this.rotation = rotation;
+            this.name = gameObject.name;
+            connections = new Hashtable();
+            switch (name)
+            {
+                case "CrossRoads":
+                    connections.Add("Up", true);
+                    connections.Add("Down", true);
+                    connections.Add("Left", true);
+                    connections.Add("Right", true);
+                    break;
+                case "StraightRoad":
+                    if (rotation / 90 >= 1)
+                    {
+                        connections.Add("Left", true);
+                        connections.Add("Right", true);
+                    }
+                    else
+                    {
+                        connections.Add("Up", true);
+                        connections.Add("Down", true);
+                    }
+                    break;
+                case "RightTurn":
+                    if(rotation == 0)
+                    {
+                        connections.Add("Down", true);
+                        connections.Add("Right", true);
+                    }
+                    else if(rotation == 90)
+                    {
+                        connections.Add("Down", true);
+                        connections.Add("Left", true);
+                    }else if(rotation == 180)
+                    {
+                        connections.Add("Up", true);
+                        connections.Add("Left", true);
+                    }else if(rotation == 270)
+                    {
+                        connections.Add("Up", true);
+                        connections.Add("Right", true);
+                    }
+                    break;
+                case "TJunction":
+                    connections.Add("Up", true);
+                    connections.Add("Down", true);
+                    connections.Add("Left", true);
+                    connections.Add("Right", true);
+                    if (rotation == 0)
+                    {
+                        connections["Left"] = false;
+                    }
+                    else if (rotation == 90)
+                    {
+                        connections["Up"] = false;
+                    }
+                    else if (rotation == 180)
+                    {
+                        connections["Right"] = false;
+                    }
+                    else if (rotation == 270)
+                    {
+                        connections["Down"] = false;
+                    }
+                    break;
+                default:
+                    break;
+            }
+              
+        }
+
+        public bool HasConnection(string direction)
+        {
+            return connections.ContainsKey(direction) ? (bool) connections[direction] : false;
+        }
+
+        public GameObject InstatiateRoad()
+        {
+            //this.position = pos;
+            //this.rotation = rotation;
+
+            this.gameObject = Instantiate(gameObject, position, Quaternion.identity);
+            this.gameObject.transform.rotation = Quaternion.Euler(0, rotation, 0);
+            return this.gameObject;
+        }
+
+        public bool WillConnect(RoadType other, string direction, string otherDirection)
+        {
+            return HasConnection(direction) && other.HasConnection(otherDirection);
+            
+        }
+
     }
 
 }
